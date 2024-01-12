865b46c2edbb593cf388704ab25635ccd2dbdf92
Robert Dargavel Smith <teticio@gmail.com>
2024-01-12 18:18:03 +0000

    formatting

--- extension.js
+++ extension.js
@@ -7,64 +7,64 @@ const path = require("path");
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-	let pythonApi;
-	try {
-		pythonApi = await PythonExtension.api();
-	} catch (error) {
-		vscode.window.showInformationMessage(
-			"Python Envy: please install the Python extension."
-		);
-		return;
-	}
+    let pythonApi;
+    try {
+        pythonApi = await PythonExtension.api();
+    } catch (error) {
+        vscode.window.showInformationMessage(
+            "Python Envy: please install the Python extension."
+        );
+        return;
+    }
 
-	const activeEditor = vscode.window.activeTextEditor;
-	if (activeEditor) {
-		await setupPythonEnvironment(activeEditor, pythonApi);
-	}
+    const activeEditor = vscode.window.activeTextEditor;
+    if (activeEditor) {
+        await setupPythonEnvironment(activeEditor, pythonApi);
+    }
 
-	let disposable = vscode.window.onDidChangeActiveTextEditor(async (editor) => {
-		if (editor) {
-			await setupPythonEnvironment(editor, pythonApi);
-		}
-	});
+    let disposable = vscode.window.onDidChangeActiveTextEditor(async (editor) => {
+        if (editor) {
+            await setupPythonEnvironment(editor, pythonApi);
+        }
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 async function setupPythonEnvironment(editor, pythonApi) {
-	let currentDir = path.dirname(editor.document.uri.fsPath);
-	const root = path.parse(currentDir).root;
+    let currentDir = path.dirname(editor.document.uri.fsPath);
+    const root = path.parse(currentDir).root;
 
-	while (currentDir !== root) {
-		const venvPath = path.join(currentDir, ".venv");
-		
-		if (fs.existsSync(venvPath) && fs.lstatSync(venvPath).isDirectory()) {
-			const currentPythonPath =
-				pythonApi.environments.getActiveEnvironmentPath();
-			const pythonPath = path.join(venvPath, "bin", "python");
+    while (currentDir !== root) {
+        const venvPath = path.join(currentDir, ".venv");
 
-			if (currentPythonPath.path !== pythonPath) {
-				try {
-					await pythonApi.environments.updateActiveEnvironmentPath(pythonPath);
-					vscode.window.showInformationMessage(
-						`Python Envy: interpreter set to: ${pythonPath}`
-					);
-				} catch (error) {
-					vscode.window.showErrorMessage(
-						`Python Envy: error setting Python interpreter: ${error.message}`
-					);
-				}
-			}
+        if (fs.existsSync(venvPath) && fs.lstatSync(venvPath).isDirectory()) {
+            const currentPythonPath =
+                pythonApi.environments.getActiveEnvironmentPath();
+            const pythonPath = path.join(venvPath, "bin", "python");
 
-			return;
-		}
-		currentDir = path.dirname(currentDir);
-	}
+            if (currentPythonPath.path !== pythonPath) {
+                try {
+                    await pythonApi.environments.updateActiveEnvironmentPath(pythonPath);
+                    vscode.window.showInformationMessage(
+                        `Python Envy: interpreter set to: ${pythonPath}`
+                    );
+                } catch (error) {
+                    vscode.window.showErrorMessage(
+                        `Python Envy: error setting Python interpreter: ${error.message}`
+                    );
+                }
+            }
+
+            return;
+        }
+        currentDir = path.dirname(currentDir);
+    }
 }
 
 function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate,
+    activate,
+    deactivate,
 };
